393bb418aa3c183e41e1e544a9becb8e7e4b4f1d
Abdullah Ghoneim <abdullahghoneim890@gmail.com>
2024-04-26 10:47:36 +0300

    fixing

--- app/index.js
+++ app/index.js
@@ -1,4 +1,3 @@
-import { Text } from "react-native";
 import Home from "../components/home";
 
 export default function Page() {
--- components/home/index.js
+++ components/home/index.js
@@ -3,25 +3,13 @@ import { Image, View, StyleSheet, Pressable, Text } from "react-native";
 import { router } from "expo-router";
 
 export default function Home() {
-  const handleImageClick = () => {
-    console.log("Image Clicked");
-  };
-
   return (
-    <View style={styles.container}>
-      {/* <Link
-        href="/mainBuilding"
-        style={{
-          width: "500",
-          height: "500",
-        }}
-      > */}
+    <View>
       <Text style={{ fontSize: 20, padding: 5 }}>
         مشروع التخرج ٢٠٢٤ الفرقة الرابعة تكنولوجيا التعليم الشعبة العامة
       </Text>
       <Pressable
         onPress={() => {
-          // "/mainBuilding"
           router.push("/mainBuilding");
         }}
       >
@@ -30,7 +18,6 @@ export default function Home() {
           source={require("../../assets/images/WhatsApp Image 2024-04-13 at 7.55.08 PM.jpeg")}
         />
       </Pressable>
-      {/* </Link> */}
     </View>
   );
 }
@@ -40,11 +27,5 @@ const styles = StyleSheet.create({
     width: 500,
     height: 500,
     borderRadius: 10,
-  },
-  container: {
-    // flex: 1,
-    // backgroundColor: "#fff",
-    // alignItems: "center",
-    // justifyContent: "center",
   },
 });
